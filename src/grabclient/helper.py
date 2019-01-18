f73a46695cc58eb7c4ebbc63f12798481a68113f
martogi.sitanggang <martogi@apps-foundry.com>
2019-01-18 12:04:27 +0700

    fixed snake_to_camel since grab was having some word to be capitalize

--- src/grabclient/helper.py
+++ src/grabclient/helper.py
@@ -2,4 +2,6 @@ import re
 
 
 def snake_to_camel(text: str) -> str:
-    return re.sub('_([a-zA-Z0-9])', lambda m: m.group(1).replace('url', 'URL').upper(), text)
+    return re.sub('_([a-zA-Z0-9])',
+                  lambda m: m.group(1).upper(),
+                  text).replace('url', 'URL').replace('id', 'ID')
