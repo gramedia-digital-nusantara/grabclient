1566b979ba3604fb87a3c12dbff017aab43985cf
martogi.sitanggang <martogi@apps-foundry.com>
2019-01-18 12:22:06 +0700

    fixed snake_to_camel

--- src/grabclient/helper.py
+++ src/grabclient/helper.py
@@ -4,4 +4,4 @@ import re
 def snake_to_camel(text: str) -> str:
     return re.sub('_([a-zA-Z0-9])',
                   lambda m: m.group(1).upper(),
-                  text).replace('url', 'URL').replace('id', 'ID')
+                  text.replace('url', 'URL').replace('id', 'ID'))
